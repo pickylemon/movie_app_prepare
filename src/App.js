23f1951d8a5744272fb96253606d746dab9ac812
pickylemon <novecento104@gmail.com>
2024-08-26 13:09:21 +0900

    Component - 선언적 렌더링과 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -9,11 +9,29 @@ import { Component } from './core/core.js'
 
 export default class App extends Component {
     constructor() {
-        super ({
-            tagName: 'h1'
+        super({
+            state: {
+                inputText: ''
+            }
         })
     }
+
+    //선언적 렌더링 : 사용할 데이터를 선언해서(ex. state의 inputText) 렌더링하는 것.
     render() {
-        this.el.textContent = 'Hello, world!'
+        this.el.classList.add('search') //클래스 이름 추가
+        this.el.innerHTML = /* html */`
+            <input>
+            <button>Click!</button>
+        `
+
+        const inputEl = this.el.querySelector('input')
+        inputEl.addEventListener('input', () => {
+            this.state.inputText = inputEl.value
+        })
+
+        const buttonEl = this.el.querySelector('button')
+        buttonEl.addEventListener('click', () => {
+            console.log(this.state.inputText)
+        })
     }
 }
