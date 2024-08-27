67836fbb6f0f470b7d8d35bd56fe208d6f5b440b
pickylemon <novecento104@gmail.com>
2024-08-28 07:54:46 +0900

    subscribe 메서드 고도화 - 여러 함수를 등록해서 상태관리 패턴 완성하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import { Component } from '../core/core.js'
 import TextField from '../components/TextField.js'
 import Message from '../components/Message.js'
+import Title from '../components/Title.js'
 
 export default class Home extends Component {
     render() {
@@ -9,7 +10,8 @@ export default class Home extends Component {
         `
         this.el.append(
             new TextField().el,
-            new Message().el
+            new Message().el,
+            new Title().el
         )
     }
 }
